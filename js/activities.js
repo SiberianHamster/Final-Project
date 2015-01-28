7d1b747ac16e70a59e9e6dd990f466e5e3e9e2ff
mryman <mark.ryman@gmail.com>
2015-01-28 12:04:05 -0800

    implements prototype for static price paragraph

--- js/activities.js
+++ js/activities.js
@@ -4,6 +4,22 @@ $downloadClicked.hide();
 
 var currentClient = [];
 
+function Client(name, email, region, season, duration, groupSize) {
+  this.name = name;
+  this.email = email;
+  this.region = region;
+  this.season = season;
+  this.duration = duration;
+  this.groupSize = groupSize;
+}
+
+Client.prototype.getPrice = function() {
+  var nepalBase = 3500
+  var baliBase = 2700
+  var thailandBase = 2250
+  return "<p class='price'>Your estimated price for this package is: $" + nepalBase + "</p>";
+  }
+
 function getInfo() {
   var $name = $('#inName').val();
   var $email = $('#inEmail').val();
@@ -13,24 +29,19 @@ function getInfo() {
   var $groupSize = $('#inGroup').val();
   var nameBuilder = $region + $season + $duration + $groupSize + ".pdf";
 
+  var client = new Client ($name, $email, $region, $season, $duration, $groupSize);
 
 
   $downloadClicked.html("<button id='download_btn'><a href='../pdf/" + nameBuilder + "'download='MyTrip.pdf'>Click here to Download</button>");
   $downloadClicked.show();
 
-  var client = new Client ($name, $email, $region, $season, $duration, $groupSize);
+
+ $(client.getPrice()).insertAfter($("#download_btn"));
+
+
 
   currentClient.push(client);
 }
 
-
-function Client(name, email, region, season, duration, groupSize) {
-  this.name = name;
-  this.email = email;
-  this.region = region;
-  this.season = season;
-  this.duration = duration;
-  this.groupSize = groupSize;
-}
 
 $submitClicked.on("click", getInfo);
