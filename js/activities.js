38c82b6073dcb79fd8b1c294a0d7aee3be27c7f0
Mark Lin <m4rklin@comcast.net>
2015-01-28 16:37:51 -0800

    Add Functional Persistance

--- js/activities.js
+++ js/activities.js
@@ -1,84 +1,100 @@
-var $submitClicked = $("#submit_btn");
-var $downloadClicked = $("#download_btn");
-$downloadClicked.hide();
+$(function() {
+  var infoRecovery = localStorage.getItem("clientInfo");
 
-var currentClient = [];
-
-function Client(name, email, region, season, duration, groupSize) {
-  this.name = name;
-  this.email = email;
-  this.region = region;
-  this.season = season;
-  this.duration = duration;
-  this.groupSize = groupSize;
-}
-
-Client.prototype.getPrice = function() {
-  var nepalBase = 3500
-  var baliBase = 2700
-  var thailandBase = 2250
-  var customerPrice
-  console.log(this.region);
-  switch(this.region){
-    case "Nepal":
-      customerPrice = nepalBase;
-      break;
-
-    case "Bali":
-      customerPrice = baliBase;
-      break;
-
-    case "Thailand":
-      customerPrice = thailandBase;
-      break;
-
-    default:
-      alert("You broke the code somehow!")
-      break;
-  }
-   return "<p class='price'>Your estimated price for this package is: $" + customerPrice + "</p>";
+  if (infoRecovery) {
+      var infoRecString = JSON.parse(infoRecovery);
+      $("#inName").val(infoRecString["clientName"]);
+      $("#inEmail").val(infoRecString["clientEmail"]);
+      $("#inRegion").val(infoRecString["region"]);
+      $("#inSeason").val(infoRecString["season"]);
+      $("#inDuration").val(infoRecString["duration"]);
+      $("#inGroup").val(infoRecString["groupSize"]);
   }
 
-function getInfo() {
-  var $name = $('#inName').val();
-  var $email = $('#inEmail').val();
-  var $region = $('#inRegion').val();
-  var $season = $('#inSeason').val();
-  var $duration = $('#inDuration').val();
-  var $groupSize = $('#inGroup').val();
-  var nameBuilder = $region + $season + $duration + $groupSize + ".pdf";
+  var $submitClicked = $("#submit_btn");
+  var $downloadClicked = $("#download_btn");
+  $downloadClicked.hide();
 
-  var client = new Client ($name, $email, $region, $season, $duration, $groupSize);
+  var currentClient = [];
 
-
-  $downloadClicked.html("<button id='download_btn'><a href='../pdf/" + nameBuilder + "'download='MyTrip.pdf'>Click here to Download</button>");
-  $downloadClicked.show();
-
-$(".price").html("");
- $(client.getPrice()).insertAfter($("#download_btn"));
-
-
-
-  currentClient.push(client);
-}
-
-
-$submitClicked.on("click", getInfo);
-
-//on blur
-{
-    var infoRecovery = {};
-
-    infoRecovery["name"] = $name
-    infoRecovery["email"] = $email
-    infoRecovery["region"] = $region
-    infoRecovery["season"] = $season
-    infoRecovery["duration"] = $duration
-    infoRecovery["groupSize"] = $groupSize
-
-
-
-    var infoRecString = JSON.stringify(infoRecovery);
-
-    localStorage.setItem("clientInfo", infoRecString);
+  function Client(name, email, region, season, duration, groupSize) {
+    this.name = name;
+    this.email = email;
+    this.region = region;
+    this.season = season;
+    this.duration = duration;
+    this.groupSize = groupSize;
   }
+
+  Client.prototype.getPrice = function() {
+    var nepalBase = 3500
+    var baliBase = 2700
+    var thailandBase = 2250
+    var customerPrice
+    console.log(this.region);
+    switch(this.region){
+      case "Nepal":
+        customerPrice = nepalBase;
+        break;
+
+      case "Bali":
+        customerPrice = baliBase;
+        break;
+
+      case "Thailand":
+        customerPrice = thailandBase;
+        break;
+
+      default:
+        alert("You broke the code somehow!")
+        break;
+    }
+     return "<p class='price'>Your estimated price for this package is: $" + customerPrice + "</p>";
+    }
+
+  function getInfo() {
+    var $name = $('#inName').val();
+    var $email = $('#inEmail').val();
+    var $region = $('#inRegion').val();
+    var $season = $('#inSeason').val();
+    var $duration = $('#inDuration').val();
+    var $groupSize = $('#inGroup').val();
+    var nameBuilder = $region + $season + $duration + $groupSize + ".pdf";
+
+    var client = new Client ($name, $email, $region, $season, $duration, $groupSize);
+
+
+    $downloadClicked.html("<button id='download_btn'><a href='../pdf/" + nameBuilder + "'download='MyTrip.pdf'>Click here to Download</button>");
+    $downloadClicked.show();
+
+  $(".price").html("");
+   $(client.getPrice()).insertAfter($("#download_btn"));
+
+
+
+    currentClient.push(client);
+  }
+
+
+  $submitClicked.on("click", getInfo);
+
+  $(".userData").blur(function(){
+    console.log("I did a blur");
+      var infoRecovery = {};
+
+
+      // infoRecovery['"' + $.each($(".userData"),function(){$(e.target).attr('id');}) + '"'] = $.each($(".userData"),function(){})
+
+      infoRecovery["clientName"] = $('#inName').val();
+      infoRecovery["clientEmail"] = $('#inEmail').val();
+      infoRecovery["region"] = $('#inRegion').val();
+      infoRecovery["season"] = $('#inSeason').val();
+      infoRecovery["duration"] =  $('#inDuration').val();
+      infoRecovery["groupSize"] = $('#inGroup').val();
+
+      var infoRecString = JSON.stringify(infoRecovery);
+      console.log(infoRecovery);
+
+      localStorage.setItem("clientInfo", infoRecString);
+  })
+});
