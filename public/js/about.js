726efec309a88f2fc59b19d08691f5dc7b92b4d7
Mark Lin <m4rklin@comcast.net>
2015-01-30 09:56:17 -0800

    toasty

--- public/js/about.js
+++ public/js/about.js
@@ -1,3 +1,17 @@
-document.getElementById("aboutCountry").addEventListener("click", function(){
-  this.className += "doAnimation";
+$(function() {
+  var codeentered = false;
+  var karray = [38,38,40,40,37,39,37,39,66,65];
+  $(document).keydown(function(event){
+    var i = ($("#kcode div").length);
+    if(event.which == karray[i]){
+      $("#kcode").append("<div class='konamic'></div>");
+      if(($("#kcode div").length) == 10){
+        codeentered = true;
+        $(".container").append('<img src="../imgs/toastie.jpg"/>');
+      };
+    }
+    else{
+      $(".konamic").remove();
+    }
+  });
 });
